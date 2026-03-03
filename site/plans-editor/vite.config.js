caf7d296a9e0a46040b7a95809301d47ff1cb295
obez-vs <grakov216500@yandex.ru>
2026-03-03 17:36:25 +0300

    sfjfg

--- site/plans-editor/vite.config.js
+++ site/plans-editor/vite.config.js
@@ -4,6 +4,10 @@ import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.NODE_ENV': JSON.stringify('production'),
+    'process.env': JSON.stringify({ NODE_ENV: 'production' }),
+  },
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.jsx'),
